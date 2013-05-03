1dcb17ff479195045d230e624d0aa67c365df244
Thomas Massmann <thomas@propertyshelf.com>
2013-05-03 11:41:01 +0200

    Added recipes common, app and zeo.
    Added dependency to python cookbook.
    Support Debian and Ubuntu OS.

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,15 @@ license          'Apache 2.0'
 description      'Installs/Configures plone'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+%w{ debian ubuntu }.each do |os|
+  supports os
+end
+
+%w{ python }.each do |cb|
+  depends cb
+end
+
+recipe "plone", "Installs a Plone CMS with ZEO Server, ZEO Client(s) and Load Balancer."
+recipe "plone::zeo", "Installs the Plone ZEO Server."
+recipe "plone::app", "Installs the Plone ZEO Client."
---
+++ recipes/commons.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: plone
+# Recipe:: commons
+#
+# Copyright 2013, Propertyshelf, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Install and configure python, python-dev, pip and virtualenv.
+include_recipe "python::default"
+
+# This package is currently necessary for the user resource (password functions).
+package "libshadow-ruby1.8" do
+  action :install
+end
