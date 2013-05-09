75f2da356a8dba2833fdecf87c19f26de58ff638
Thomas Massmann <thomas@propertyshelf.com>
2013-05-09 13:19:19 +0200

    Added chefspec test for libshadow installation.

--- spec/commons_spec.rb
+++ spec/commons_spec.rb
@@ -1,8 +1,10 @@
 require 'chefspec'
 
-describe 'plone::commons' do
+describe 'The recipe plone::commons' do
   let (:chef_run) { ChefSpec::ChefRunner.new.converge 'plone::commons' }
-  it 'should do something' do
-    pending 'Your recipe examples go here.'
+
+  it 'should install package libshadow' do
+    expect(chef_run).to install_package 'libshadow-ruby1.8'
   end
+
 end
