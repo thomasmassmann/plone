b94e809575fe71b2cf356f1a657ec68d3bae3fec
Thomas Massmann <thomas@propertyshelf.com>
2013-06-12 09:30:36 +0200

    Reset tests for commons recipe.

--- spec/commons_spec.rb
+++ spec/commons_spec.rb
@@ -1,10 +1,8 @@
 require 'chefspec'
 
-describe 'The recipe plone::commons' do
+describe 'plone::commons' do
   let (:chef_run) { ChefSpec::ChefRunner.new.converge 'plone::commons' }
-
-  it 'should install package libshadow' do
-    expect(chef_run).to install_package 'libshadow-ruby1.8'
+  it 'should do something' do
+    pending 'Your recipe examples go here.'
   end
-
 end
